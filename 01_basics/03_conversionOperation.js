c4b89b246b16fdcbadf92dcf6b6b982f4a3b218b
Rima Rajendran M K <44948540+Rima-Rajendran-MK@users.noreply.github.com>
2024-02-24 18:34:14 +0000

    coversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = 33;
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = true;
+
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+//console.log(boolenIsLoggedIn); 
+
+//1 => true; 0 => false
+//"" => false
+//"Rima" => true
+
+let someNumber = 33;
+
+let strinNumber = String(someNumber);
+console.log(strinNumber); // => 33
+console.log(typeof strinNumber); // => string
